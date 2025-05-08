ed078f1e5a0298fce98d99cf6c3e64b52f47cc51
codingjs12 <202245068@itc.ac.kr>
2025-05-08 22:42:51 +0900

    250508 회원가입

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import {useState} from 'react';
 import { TextField, Button, Container, Typography, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 function Join() {
+
+  const [email, setEmail] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+
+  const join = () => {
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      alert('올바른 이메일 형식을 입력해주세요.');
+      return;
+    }
+
+    if (password !== passwordConfirm) {
+      alert('비밀번호가 일치하지 않습니다.');
+      return;
+    }
+
+
+    fetch('http://localhost:3000/user/join', {
+      method : 'POST',
+      headers : {
+        'Content-Type': 'application/json',
+      },
+      body : JSON.stringify({ email, password, nickname })
+    })
+  .then(res => res.json())
+  .then(data => {
+    alert(data.message);
+  })
+}
+
+
   return (
     <Container maxWidth="xs">
       <Box
@@ -15,16 +50,42 @@ function Join() {
         <Typography variant="h4" gutterBottom>
           회원가입
         </Typography>
-        <TextField label="Username" variant="outlined" margin="normal" fullWidth />
-        <TextField label="Email" variant="outlined" margin="normal" fullWidth />
+        <TextField label="이메일"
+         variant="outlined"
+         margin="normal"
+         fullWidth
+         value={email}
+         onChange={(e) => setEmail(e.target.value)}
+          />
+        <TextField label="닉네임"
+         variant="outlined"
+         margin="normal"
+         fullWidth
+         value={nickname}
+         onChange={(e) => setNickname(e.target.value)} 
+         />
         <TextField
-          label="Password"
+          label="비밀번호"
           variant="outlined"
           margin="normal"
           fullWidth
           type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <Button variant="contained" color="primary" fullWidth style={{ marginTop: '20px' }}>
+        <TextField
+          label="비밀번호 확인"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          type="password"
+          value={passwordConfirm}
+          onChange={(e) => setPasswordConfirm(e.target.value)}
+        />
+        <Button variant="contained"
+         color="primary"
+         fullWidth style={{ marginTop: '20px' }}
+         onClick={() => { join() }}>
             회원가입
         </Button>
         <Typography variant="body2" style={{ marginTop: '10px' }}>
